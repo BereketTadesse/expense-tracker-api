f03e9d17b13ce3c936883d3b087787b7dfc45ab0
BereketTadesse <berekettadesse1244@gmail.com>
2026-08-20 16:51:25 +0300

    feat: initialize application with CORS, global validation pipes, and API prefix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: [
+      'https://expense-tracker-ethio.vercel.app',
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+    ],
     credentials: true,
   });
 
